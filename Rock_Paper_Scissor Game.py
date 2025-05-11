51eb186c593dd5e722d39c2c6c6fba06bcdc2956
Teja-Janga <tejat7927@gmail.com>
2025-05-11 17:10:14 +0530

    Add files via upload

---
+++ Rock_Paper_Scissor Game.py
@@ -0,0 +1,53 @@
+# Ask user to make a choice
+# If choice is not valid
+#   Print an Error Message
+# Let the computer make a choice
+# Print the choices of both user and computer (emojis)
+# Determine the winner
+# Ask the user if  they want to continue
+# If not
+#   Terminate
+
+
+#In modules "random", "randint" is to generate random numbers between a range of two distinct numbers.
+# But with "choice" we can select a random thing from multiple choices
+
+import random
+
+# In Dictionary Key -> 'Value'
+# 'r' -> "🪨"
+# 'p' -> "📃"
+# 's' -> "✂️"
+
+
+#Ask user to make Choice
+choices = ("r", "p", "s")
+emoji = {'r' : "🪨", 'p' : "📃", 's' : "✂️"}
+
+
+while True:
+# Print Error Message if Choice is Invalid
+    user = input("Rock, Paper, Scissors? (r/p/s): ").lower()
+    if user not in choices:
+        print("Invalid Choice")
+        continue
+
+# Let Computer make a choice and print both user and computer's Choices
+    computer = random.choice(choices)
+    print(f'You Chose {emoji[user]}')
+    print(f'Computer Chose {emoji[computer]}')
+
+    #Determining The Winner
+    if user == computer:
+        print("Tie!")
+    elif ((user == 'r' and computer == 's') 
+    or (user == 'p' and computer == 'r') 
+    or (user == 's' and computer == 'p')):
+        print("You win!")
+    else:
+        print("You lose!")
+
+    #Asking the user if they want to continue
+    should_continue = input('Continue? (Y/N): ').lower()
+    if should_continue == 'n':
+        break
